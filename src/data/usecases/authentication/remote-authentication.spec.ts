acfeaf10ab0bb4a1e223b38596d7aa71efbf48eb
thsthiago <thiagocabral477@gmail.com>
2022-04-23 18:47:51 -0300

    test: ensure RemoteAuthentication calls HttpPostClient white correct body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import faker from '@faker-js/faker'
+import { mockAuthtication } from '@/domain/test/mock-authentication'
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -21,7 +22,14 @@ describe('RemoteAuthentication', () => {
   it('Should call HttpClient with correct URL', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
-    await sut.auth()
+    await sut.auth(mockAuthtication())
     expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  it('Should call HttpClient with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const authenticationParams = mockAuthtication()
+    await sut.auth(authenticationParams)
+    expect(httpPostClientSpy.body).toEqual(authenticationParams)
   })
 })
